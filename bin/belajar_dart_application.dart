420594109849d0930c1b780fc087b47273c5e5cb
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-10-28 10:22:19 +0700

    .

--- bin/belajar_dart_application.dart
+++ bin/belajar_dart_application.dart
@@ -1,5 +1,1 @@
-import 'package:belajar_dart_application/belajar_dart_application.dart' as belajar_dart_application;
-
-void main(List<String> arguments) {
-  print('Hello world: ${belajar_dart_application.calculate()}!');
-}
+void main() {}
